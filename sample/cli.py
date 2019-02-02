2178cff649e17d09bf7f51aa55d5d5bbfb108aea
rachidnajjar <rachid.najjar@sfr.fr>
2019-02-02 18:20:24 +0100

    Nouveau script utilisant click

--- sample/cli.py
+++ sample/cli.py
@@ -7,12 +7,11 @@ Created on 31 déc. 2018
 import click
 import requests
 
-
 SAMPLE_API_KEY = 'b1b15e88fa797225412429c1c50c122a1'
 
 @click.command()
-@click.option("--api-key", "-a", help="your API key for the OpenWeatherMap API")
-@click.argument("location")
+@click.argument('location')
+@click.option('--api-key', '-a', help='your api key for OpenWeatherMap')
 def main(location,api_key):
     '''
     A little weather tool that shows you the current weather in a location of
@@ -23,19 +22,13 @@ def main(location,api_key):
     You need a valid API key from OpenWeatherMap for the tool to work. You can
     sign up for a free account at https://openweathermap.org/appid.
     '''
-    weather = current_weather(location)
-    print("The weather in {0} right now: {1}.".format(location, weather))
+    weather = getWeather(location)
+    print('The weather in {0} right now: {1}.'.format(location, weather))
 
-def current_weather(location, api_key=SAMPLE_API_KEY):
+def getWeather(location, api_key=SAMPLE_API_KEY):
     url = "http://samples.openweathermap.org/data/2.5/weather"
-
-    query_params = {
-        'q': location,
-        'appid': api_key,
-    }
-
+    query_params = { 'q': location, 'appid': api_key, }
     response = requests.get(url, params=query_params)
-
     return response.json()['weather'][0]['description']
 
 if __name__ == '__main__':
---
+++ sample/dbader.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2 févr. 2019
+
+@author: rachid
+'''
+import click
+
+@click.command()
+def main():
+    print("Je suis la fonction main du script dbader")
+
+if __name__ == '__main__':
+    main()
